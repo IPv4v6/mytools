45c872380549cb802359f840c0133be79bfbfd47
Stefan <mail.ipv4v6+gh@gmail.com>
2024-04-26 00:26:15 +0200

    add script to checkout all revisions of a file in a git repository

---
+++ git/checkout-all-file-revs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z ${1} ]]; then
+	echo "Usage: ${0} [file name]"
+	exit 0
+fi
+
+for REV in $(git log --diff-filter=ACMRd --format=%H -- "${1}")
+do
+	DATE=$(git log -1 --format=%as "${REV}")
+	git checkout "${REV}" "${1}"
+	mv -i "${1}" "${1}","${DATE}","${REV}"
+done
